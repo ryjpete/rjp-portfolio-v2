5f01056050aba21463d3a90050da38d83f0ca59c
ryjpeterson <ryan.j.peterson.80@gmail.com>
2021-03-24 14:42:06 -0500

    author novel sections; responsive

--- src/components/sectNovel/sectNovel.js
+++ src/components/sectNovel/sectNovel.js
@@ -1,16 +1,65 @@
 import React from 'react'
+// import { GatsbyImage } from 'gatsby-plugin-image'
 
 // Import component styles
 import styles from './sectNovel.module.less'
 
+// Import Images
+import imgGridYellow from '../../images/gridYellow.jpg'
+import imgGridGreen from '../../images/gridGreen.jpg'
+import imgGridRed from '../../images/gridRed.jpg'
+
+const hexToRgb = ( hex ) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null
+}
+
+const setTextColor = ( hexColor ) => {
+  const textColor = hexToRgb(hexColor)
+  // console.log(textColor)
+  const yiq = ((textColor.r * 299) + (textColor.g * 587) + (textColor.b + 114)) / 1000
+  
+  return (yiq >= 128) ? '#000000' : '#ffffff'
+}
+
 const Novel = ( props ) => {
   const title = props.title.split(' ')
   const saga = props.package1.split(' ')
+  const addClass = props.title.match(/\b(\w)/g).join('').toLowerCase()
+  let gridImage
+
+  switch (title[4]) {
+    case 'Humanity':
+      gridImage = imgGridYellow
+      // gridImage = 'gridYellow'
+      // import gridImage from '../../images/gridYellow.jpg'
+      break
+    case 'Greed':
+      gridImage = imgGridGreen
+      // gridImage = 'gridGreen'
+      // import gridImage from '../../images/gridGreen.jpg'
+      break
+    case 'Chaos':
+      gridImage = imgGridRed
+      // gridImage = 'gridRed'
+      // import gridImage from '../../images/gridRed.jpg'
+      break
+    default:
+      return false
+  }
 
   return (
     <section
-      className={`${styles.novel} ${styles[props.mode]}`}
-      style={{ backgroundColor: '#' + props.mainColor }}
+      className={`${styles.novel} ${styles[addClass]}`}
+      style={{
+        backgroundColor: '#' + props.mainColor,
+        color: setTextColor('#' + props.mainColor),
+      }}
     >
       {/* Title */}
       <div className={styles.title}>
@@ -51,11 +100,6 @@ const Novel = ( props ) => {
           xmlns='http://www.w3.org/2000/svg'
           version='1.1'
           viewBox='0 0 100 100'
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
         >
           <defs>
             <clipPath
@@ -74,7 +118,7 @@ const Novel = ( props ) => {
             clipPath="url(#blackBox)" />
         </svg>
 
-        <figure style={{ margin: 0, }}>
+        <figure>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             version='1.1'
@@ -95,7 +139,7 @@ const Novel = ( props ) => {
               width='100%'
               height='100%'
               preserveAspectRatio='xMinYMin slice'
-              xlinkHref={props.gridImage}
+              xlinkHref={gridImage}
               clipPath='url(#user-space)' />
           </svg>
         </figure>
@@ -103,24 +147,21 @@ const Novel = ( props ) => {
 
       {/* Content */}
       <div className={styles.content}>
-        <div className={styles.left}>
-          <div className={styles.package}>
-            <p>
-              {saga[0]}<br />
-              {saga[1]}<br />
-              {saga[2]}<br />
-              <span className={styles.emp}>
-                {props.package1Num}
-              </span>
-            </p>
-          </div>
-          <div className={styles.package}>
-            {props.package2}
-          </div>
+        <div className={styles.package}>
+          <p>
+            {saga[0]}<br />
+            {saga[1]}<br />
+            {saga[2]}<br />
+            <span className={styles.emp}>
+              {props.package1Num}
+            </span>
+          </p>
         </div>
-        <div className={styles.right}>
-          {props.tease}
+        <div className={styles.package}>
+          {props.package2}
         </div>
+
+        {props.tease}
       </div>
     </section>
   )
--- src/pages/author.js
+++ src/pages/author.js
@@ -10,9 +10,6 @@ import PageHeader from '../components/pageHeader/pageHeader'
 import Novel from '../components/sectNovel/sectNovel'
 import SEO from '../components/seo/seo'
 
-// Import Images
-import imgGridYellow from '../images/gridYellow.jpg'
-
 // Setting the rendering options
 const options = {
   renderText: text => text.split('\n').flatMap((text, i) => [i > 0 && <br key={i} />, text])
@@ -23,51 +20,28 @@ const Author = ({ data }) => (
     <SEO title='Home of the Humanities Saga' />
 
     <PageHeader
-      addClass='author'
-      bgcolor='#202020'
+      addClass='fH'
+      bgcolor='#000000'
+      color='#FFFFFF'
     >
-      <h1 style={{
-        color: '#ffffff',
-        marginBottom: '7rem',
-      }}>
-        The official site of author<br />
-        Ryan Jennings Peterson.
-      </h1>
-      <p style={{
-        color: '#ffffff',
-        marginLeft: '30rem',
-      }}>
-        Welcome to the official website of indie author Ryan Jennings Peterson, creator of Jolene Hartley and the thrilling Humanities Saga. The action-packed novels, which debuted with On the Ladder of Humanity and was followed closely by On the Edge of Greed, charges on with book three, On the Road Through Chaos, an intense continuation into the turbulent force that is Jolene Hartley.
-      </p>
+      {data.contentfulPage.pageTitle && renderRichText(data.contentfulPage.pageTitle, options)}
     </PageHeader>
 
-    {data.allContentfulNovel.edges.map(novel => {
-      // if ( novel.node.gridImage ) {
-      //   novel.node.gridImage.gatsbyImageData.images.sources.map(img => {
-      //     return (
-      //       <p>{img.srcSet}</p>
-      //     )
-      //   }
-      // }
-
-      return (
-        <Novel
-          key={novel.node.contentful_id}
-          title={novel.node.title}
-          package1={novel.node.seriesTitle1}
-          package1Num={novel.node.bookNumber}
-          package2={renderRichText(novel.node.seriesTitle2, options)}
-          mode='dark'
-          mainColor={novel.node.mainColor}// used for bgColor
-          textColor={novel.node.textColor}// used for non-title font color
-          subColor1={novel.node.subColor1}// used for title sub1
-          subColor2={novel.node.subColor2}// used for title sub2
-          subColor3={novel.node.subColor3}// used for title sub3
-          titleColor1={novel.node.titleColor1}// used for title main1
-          tease={renderRichText(novel.node.tease, options)} />
-          // gridImage={novel.node.gridImage.gatsbyImageData.images.sources[0].srcSet} />
-      )
-    })}
+    {data.allContentfulNovel.edges.map(novel => (
+      <Novel
+        key={novel.node.contentful_id}
+        title={novel.node.title}
+        package1={novel.node.seriesTitle1}
+        package1Num={novel.node.bookNumber}
+        package2={renderRichText(novel.node.seriesTitle2, options)}
+        mainColor={novel.node.mainColor}// used for bgColor
+        textColor={novel.node.textColor}// used for non-title font color
+        subColor1={novel.node.subColor1}// used for title sub1
+        subColor2={novel.node.subColor2}// used for title sub2
+        subColor3={novel.node.subColor3}// used for title sub3
+        titleColor1={novel.node.titleColor1}// used for title main1
+        tease={renderRichText(novel.node.tease, options)} />
+    ))}
   </Layout>
 )
 
@@ -100,6 +74,13 @@ export const pageQuery = graphql`
             gatsbyImageData(layout: FULL_WIDTH)
           }
         }
+      }
+    }
+    contentfulPage(contentful_id: {eq: "5jqp6cNBeRptjYLkR8UIaH"}) {
+      pageName
+      pageSlug
+      pageTitle {
+        raw
       }
     }
   }
